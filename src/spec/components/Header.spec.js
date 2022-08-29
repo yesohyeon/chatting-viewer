8e9442ea26aacd6d8396bd25a421fe90a94f290a
Sohyeon <needtlc13@gmail.com>
2022-08-29 22:47:56 +0900

    test: add test for Header and ChatroomItem components

---
+++ src/spec/components/Header.spec.js
@@ -0,0 +1,39 @@
+import { screen, fireEvent } from "@testing-library/react";
+import { useLocation } from "react-router-dom";
+
+import Header from "../../components/Header/Header";
+
+import renderWithProviders from "../utils/renderWithProviders";
+
+const PathLocation = () => {
+  const location = useLocation();
+
+  return <div>{location.pathname}</div>;
+};
+
+describe("<Header />", () => {
+  it("Header component show two links", () => {
+    renderWithProviders(<Header />);
+
+    const icons = screen.getAllByRole("link");
+    expect(icons.length).toBe(2);
+  });
+
+  it("Move to friends list", () => {
+    renderWithProviders(<><Header /><PathLocation /></>);
+
+    const icons = screen.getAllByRole("link");
+    fireEvent.click(icons[0]);
+
+    expect(screen.getByText("/friends")).toBeInTheDocument();
+  });
+
+  it("Move to chatroom list", () => {
+    renderWithProviders(<><Header /><PathLocation /></>);
+
+    const icons = screen.getAllByRole("link");
+    fireEvent.click(icons[1]);
+
+    expect(screen.getByText("/rooms")).toBeInTheDocument();
+  });
+});
