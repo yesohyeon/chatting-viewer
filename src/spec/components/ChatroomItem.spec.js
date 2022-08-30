1ca02909d52cc64fdf7164a97a730250069da38c
Sohyeon <needtlc13@gmail.com>
2022-08-30 10:08:54 +0900

    refactor: add handleClick to two tests

--- src/spec/components/ChatroomItem.spec.js
+++ src/spec/components/ChatroomItem.spec.js
@@ -1,14 +1,7 @@
 import { screen, fireEvent } from "@testing-library/react";
-import { useLocation } from "react-router-dom";
 
 import ChatroomItem from "../../components/Rooms/ChatroomItem";
 import renderWithProviders from "../utils/renderWithProviders";
-
-const PathLocation = () => {
-  const location = useLocation();
-
-  return <div>{location.pathname}</div>;
-};
 
 describe("<ChatroomItem />", () => {
   it("ChatroomItem component should show correct contents which are consistent with props", () => {
@@ -17,27 +10,22 @@ describe("<ChatroomItem />", () => {
     const name = "friend name";
     const lastMessage = "last message";
     const createdAt = "2022-08-20T09:48:00.000Z"
+    const handleClick = jest.fn();
 
     renderWithProviders(
-      <>
-        <ChatroomItem
-          id={id}
-          profile={profile}
-          name={name}
-          lastMessage={lastMessage}
-          createdAt={createdAt}
-        />
-        <PathLocation />
-      </>
+      <ChatroomItem
+        id={id}
+        profile={profile}
+        name={name}
+        lastMessage={lastMessage}
+        createdAt={createdAt}
+        handleClick={handleClick}
+      />
     );
 
     expect(screen.getByRole("img")).toHaveAttribute("src", profile);
     expect(screen.getByText("friend name")).toBeInTheDocument();
     expect(screen.getByText("last message")).toBeInTheDocument();
     expect(screen.getByText("2022-08-20 09:48")).toBeInTheDocument();
-
-    fireEvent.click(screen.getByRole("img"));
-
-    expect(screen.getByText(`/rooms/${id}`)).toBeInTheDocument();
   });
 });
